35266f73393cc163857c520fe474511f77533b7f
carl Helberg <carlhelberg.se@gmail.com>
2018-05-07 17:44:34 +0200

    Initial skeleton and basic frame

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  	<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+    <!--Font Awesome-->
+    <link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel = "stylesheet" href = "css/style.css">
+    <title>Shawpify</title>
+</head>
+<body>
+    <!-- header-->
+    <header>
+      <div class = "container-fluid">
+        <div class = "jumbotron text-center">
+          <h1>Shawpify</h1>
+          <p>Offering a wide range of products from consumer electronics to cars</p>
+        </div>
+      </div>
+    </header>
+    <nav class = "navbar navbar-inverse navbar-fixed-top">
+      <div class = "container-fluid">
+        <header class = "navbar-header">
+          <button type = "button" class ="navbar-toggle" data-toggle = "collapse" data-target = "#myNavbar">
+            <!-- Add manual glyphicon-hamberger one bar at a time-->
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+
+          </button>
+          <a class = "navbar-brand" href = "index.php">Shawpify Home </a>
+        </header>
+      <div class ="collapse navbar-collapse" id ="myNavbar">
+        <ul class = "nav navbar-nav">
+          <li><a href = "#">Products</a></li>
+          <li><a href ="#">Deals</a></li>
+        </ul>
+        <ul class = " nav navbar-nav navbar-right">
+          <li><a href = "#"><span class = "glyphicon glyphicon-user"></span> Sign In</a></li>
+          <li><a href = "#"><span class = "glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+          <li><form class = "navbar-form" action = "#" method ="POST">
+            <input type ="text"class = "form-control" placeholder = "Search for a product">
+            <button type = "submit" class = "btn btn-primary"><span class = "glyphicon glyphicon-search s"></span></button>
+          </form></li>
+        </ul>
+      </div>
+    </div>
+    </nav>
+    <!--Main-->
+  <main>
+    <div class = "container-fluid">
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+      <p>Hello, world!</p>
+    </div>  
+  </main>
+
+
+
+    <!-- footer -->
+  <footer class = "text-center">
+    <nav class = "navbar navbar-inverse">
+      <div class = "container-fluid">
+        <ul class = " nav navbar-nav">
+          <li class = "navbar-text"> Copyright &copy; Someone</li>
+          <li><a href = "#"><i class = "fa fa-facebook"></i></a></li>
+          <li><a href = "#"><i class = "fa fa-google"></i></a></li>
+          <li><a href = "#"><i class = "fa fa-twitter"></i></a></li>
+        </ul>
+        <ul class = "nav navbar-nav navbar-right">
+        <li><a href = "#">Site Map</a></li>
+        <li><a href = "#">Contact</a></li>
+        <li><a href = "#">Terms &amp; Conditions</a></li>
+        </ul>
+      </div>
+    </nav>
+  </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
